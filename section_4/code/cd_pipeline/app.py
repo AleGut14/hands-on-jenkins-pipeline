fb232678a96b9151e77c40cd9667ce860b70ee9c
AleGut14 <alessiamguttadauro@gmail.com>
2025-04-15 12:05:14 +0200

    Update app.py
    Update gifs file

--- section_4/code/cd_pipeline/app.py
+++ section_4/code/cd_pipeline/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 # list of cat images
 images = [
     "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDh2ajd5bDlyZ2RtejZ0NndwcjQwb2dyeGl4bXN0M2NldjQzbjY3MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/CjmvTCZf2U3p09Cn0h/giphy.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-25329-1381845415-0.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-23859-1381845509-0.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-19708-1381845008-7.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-27162-1381845360-0.gif"
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjljdTk3eTF1dzMwd2Y0cWtlbmtiOGRkNnFqNXF1eG5jZHZ2dzJyNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MDJ9IbxxvDUQM/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/mlvseq9yvZhba/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lJNoBCvQYp7nq/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/8vQSQ3cNXuDGo/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/BBNYBoYa5VwtO/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/13CoXDiaCcCoyk/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdxN3gxbXF1cGh1dXVrZWJrbXBreG1yZGk3ZGpqYncwNzIzMHhlYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/q1MeAPDDMb43K/giphy.gif",
+    "https://media.giphy.com/media/B7ppUExX92PjW/giphy.gif?cid=ecf05e47yox8c8s6viab2979vbw8kkt7ddvkapjat3a04tt8&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/12PA1eI8FBqEBa/giphy.gif?cid=ecf05e47ee89hrgzb5f5ow6hpf3jv3ochvowwrg0tkz2yh69&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/CqVNwrLt9KEDK/giphy.gif?cid=ecf05e47ee89hrgzb5f5ow6hpf3jv3ochvowwrg0tkz2yh69&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/6C4y1oxC6182MsyjvK/giphy.gif?cid=ecf05e477p2kt2x6egjhx7nxs7wa4thplqvfi90xqriwzx12&ep=v1_gifs_search&rid=giphy.gif&ct=g"
 ]
 
 @app.route('/')
